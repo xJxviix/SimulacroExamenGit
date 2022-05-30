41a2477d671bb816bc421098e532fd0abdb97ac7
xJxviix <jxvi21@icloud.com>
2022-05-30 18:14:44 +0200

    añadido mejora en metodo de borrar Datos.

--- SimulacroExamenGit/Form1.cs
+++ SimulacroExamenGit/Form1.cs
@@ -24,6 +24,7 @@ namespace SimulacroExamenGit
 
         private void btnBorrar_Click(object sender, EventArgs e)
         {
+            labelResultado.Text = "";
             textBoxNombre.Text = "";
             textBoxApellido.Text = "";
             textBoxEdad.Text = "";
