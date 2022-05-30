6c9d48fd7bc11bebd4d6101d97349e3bdae37f43
xJxviix <jxvi21@icloud.com>
2022-05-30 17:57:01 +0200

    Añadido mostrar la edad en el label al presionar el boton de Enviar

--- SimulacroExamenGit/Form1.cs
+++ SimulacroExamenGit/Form1.cs
@@ -19,7 +19,7 @@ namespace SimulacroExamenGit
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
-            labelResultado.Text = "Tu nombre es: " + textBoxNombre.Text + " " + textBoxApellido.Text;
+            labelResultado.Text = "Tu nombre es: " + textBoxNombre.Text + " " + textBoxApellido.Text + " Tu edad es de: " + textBoxEdad.Text;
         }
     }
 }
